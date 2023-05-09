a14f61b79f663d114f68a83affd40a3760f44354
Guilherme de Almeida Martins <54013549+Guil-Baka@users.noreply.github.com>
2023-05-09 09:21:58 -0300

    Sem paciencia pra terminar :)

---
+++ Aulas/09-05-23/funcCalcTempLivre.js
@@ -0,0 +1,13 @@
+export function calcTempLivre() {
+
+  alert("checkDebug");
+  let tempoEstudoSemExt = 0;
+  let tempoSemanalLivre = 0;
+  
+  let resultado = tempoSemanalLivre / tempoLivre;
+  
+  tempoEstudoSemExt = window.prompt(texto,valor);
+  tempoSemanalLivre = window.prompt(texto,valor);
+  
+  let tempoLivre = tempoEstudoSemExt + tempoSemanalLivre;
+}
